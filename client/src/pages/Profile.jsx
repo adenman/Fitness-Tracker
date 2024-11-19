5ecf7d3b8dd1e926c9f558d973e03c17838fbbd3
Aden Neal <adenocbenji@gmail.com>
2024-11-19 15:36:09 -0600

    fixed userName input box

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -227,7 +227,7 @@ export default function Profile({ onLogout }) {
           type="text" 
           className="w-full px-3 py-2 text-black border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
           placeholder="New Username" 
-          value={data.oneUser.userName} 
+          value={newName} 
           onChange={(e) => setNewName(e.target.value)} 
           onKeyPress={handleKeyPress}
         />
