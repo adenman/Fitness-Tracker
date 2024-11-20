dad92a4d095dcd832385c243740912de5137cb92
Aden Neal <adenocbenji@gmail.com>
2024-11-20 13:05:05 -0600

    gh

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider} from "react-router-dom";
 import "./index.css";
-import './app.css'
+import './App.css'
 
 
 import App from "./App";
