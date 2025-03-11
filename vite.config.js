698b700a5bddb2e7b1ef4c950e5ff00fb19775a6
Aden Neal <adenocbenji@gmail.com>
2025-03-11 14:54:19 -0500

    update

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,36 @@
+// import { defineConfig } from 'vite'
+
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//     external: ["react", "react-dom"]
+//     }
+//   },
+//   resolve: {
+//     alias: {
+//       'react': './node_modules/react',
+//       'react-dom': './node_modules/react-dom'
+//     }
+//   }
+// })
+
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
-    external: ["react", "react-dom"]
-    }
+      input: {
+        main: './index.html',
+      },
+    },
   },
-  resolve: {
-    alias: {
-      'react': './node_modules/react',
-      'react-dom': './node_modules/react-dom'
-    }
-  }
+  base: './'
 })
 
